8db807d97eec8a6b42f3b91a17a9c63f82c2181f
Sander-riis <74243825+Sander-riis@users.noreply.github.com>
2021-02-08 17:16:34 +0100

    iter 2

--- AI/OCR.py
+++ AI/OCR.py
@@ -2,25 +2,35 @@ import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('KVITT2.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-grayImg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-adaptive_trhesholdImg = cv2.adaptiveThreshold(grayImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 85, 11)
+img = cv2.imread('vy.jpg')
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+adaptive_thresholdImg = cv2.adaptiveThreshold(grayImg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 77, 14)
 
+
+NOR = 'nor'
 config = "--psm 4"
 hImg, wImg, _ = img.shape
-boxes = pytesseract.image_to_data(adaptive_trhesholdImg, lang='nor', config=config)
+boxes = pytesseract.image_to_data(adaptive_thresholdImg, lang=NOR, config=config)
+
+
+# #[   0          1           2           3           4          5         6       7       8        9        10      11  ]
+# #['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text']
 print(pytesseract.get_languages(config=''))
 print(boxes)
 
+detectedWords = []
 for x, b in enumerate(boxes.splitlines()):
     if x != 0:
         b = b.split()
         print(b)
         if len(b) == 12:
             x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
-            cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 255), 3)
-            cv2.putText(img, b[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 255), 2)
+            cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 255), 1)
+            cv2.putText(img, b[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (25, 25, 255), 1)
+            detectedWords.append(b[11])
 
-cv2.imshow('result', adaptive_trhesholdImg)
+print(detectedWords)
+cv2.imshow('adaptive_img', adaptive_thresholdImg)
+cv2.imshow('result', img)
 cv2.waitKey(0)

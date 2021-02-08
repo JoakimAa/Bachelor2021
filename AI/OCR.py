3716534c8e50b35654d055c9a0b66f9fcd2f2283
Sander-riis <74243825+Sander-riis@users.noreply.github.com>
2021-02-08 13:06:57 +0100

    OCR

--- AI/OCR.py
+++ AI/OCR.py
@@ -2,14 +2,17 @@ import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('Capture.PNG')
+img = cv2.imread('KVITT2.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+grayImg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+adaptive_trhesholdImg = cv2.adaptiveThreshold(grayImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 85, 11)
 
+config = "--psm 4"
 hImg, wImg, _ = img.shape
-boxes = pytesseract.image_to_data(img, lang='osd')
+boxes = pytesseract.image_to_data(adaptive_trhesholdImg, lang='nor', config=config)
 print(pytesseract.get_languages(config=''))
-print(pytesseract.image_to_string(img))
 print(boxes)
+
 for x, b in enumerate(boxes.splitlines()):
     if x != 0:
         b = b.split()
@@ -19,5 +22,5 @@ for x, b in enumerate(boxes.splitlines()):
             cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 255), 3)
             cv2.putText(img, b[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 255), 2)
 
-cv2.imshow('result', img)
+cv2.imshow('result', adaptive_trhesholdImg)
 cv2.waitKey(0)

e6085640cd3aff385a6d2003ed2e5c2e44fdc042
Sander-riis <74243825+Sander-riis@users.noreply.github.com>
2021-05-11 09:52:52 +0200

    meh

--- ML/NER/ner_spacy.py
+++ ML/NER/ner_spacy.py
@@ -1,8 +1,20 @@
 import spacy
+from spacy.lang.nb.examples import sentences
+from spacy import displacy
 
+<<<<<<< Updated upstream
 text_file = open("TextInput/rawText_neptun.txt", "r", encoding="utf-8")
+=======
+
+nlp = spacy.load("nb_core_news_sm")
+
+text_file = open("TextInput/rawText.txt", "r", encoding="UTF-8", )
+>>>>>>> Stashed changes
 text = text_file.read()
 text_file.close()
+
+
+
 
 print("-----------BEFORE JOIN--------------")
 print(text)
@@ -12,10 +24,30 @@ print(text)
 #print("-----------AFTER JOIN--------------")
 #print(text)
 
+<<<<<<< Updated upstream
 nlp = spacy.load("pl_core_news_md")
+=======
+>>>>>>> Stashed changes
 
 print("-----------SPACY OUTPUT--------------")
 doc = nlp(text)
+#displacy.serve(doc)
+for ent in doc.ents:
+    #print(ent.text, ent.label_)
+    if(ent.label_ == "MISC"):
+        print(ent.text)
 
-for entity in doc.ents:
-    print(entity.text, entity.label)
+displacy.serve(doc, style='dep')
+for doc in doc.ents:
+    for ent in doc:
+        #print(token.ent_type_)
+        if ent.label_ == "MISC":
+            subj = [w for w in ent.head if w.dep_ == "flat:name"]
+            if subj:
+                print(subj[0], "-->", ent.text)
+            # We have a prepositional object with a preposition
+        elif ent.dep_ == "noun" and ent.head.dep_ == "flat:name":
+            print(ent.head.head, "-->", ent.text)
+
+
+
